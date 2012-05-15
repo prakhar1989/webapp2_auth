49344e46b06f1d2c1e06df0df76d414693a1c80e
prakhar1989 <prakhar1989@gmail.com>
2012-05-15 23:05:04 +0530

    first commit

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
+import os
+import sys
+import jinja2
 import webapp2
 import config
 from webapp2_extras import auth
-from webapp2_extras.routes import RedirectRoute
 from webapp2_extras import sessions
 from webapp2_extras.auth import InvalidAuthIdError
 from webapp2_extras.auth import InvalidPasswordError
 
-from webapp2_extras.appengine.auth.models import User
-from google.appengine.ext.ndb import model
-
-# class CustomUser(User):
-#     email = model.StringProperty(required=True)
-
 def user_required(handler):
-    """
-    Simple decorator to check if a user's associated with the current
-    session. Will fail if there's no session present
-    """
     def check_login(self, *args, **kwargs):
         auth = self.auth
         if not auth.get_user_by_session():
-            # If handler has no login_url specified invoke a 403 error
-            try:
-                self.redirect(self.auth_config['login_url'], abort=True)
-            except (AttributeError, KeyError), e:
-                self.abort(403)
+            self.redirect(self.auth_config['login_url'])
         else:
             return handler(self, *args, **kwargs)
 
     return check_login
 
-# auth.default_config = {
-#     # "user_model" : CustomUser,
-#     'cookie_name' : 'testauth'
-# }
-
 class BaseHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
     def dispatch(self):
         try:
@@ -59,61 +51,73 @@ class BaseHandler(webapp2.RequestHandler):
             'logout_url' : self.uri_for('logout')
         }
 
+    def set_flash(self, msg):
+        self.session_store.get_session()['_flash'] = msg
+
+    def get_flash(self):
+        flash_msg = self.session_store.get_session()['_flash']
+        del self.session_store.get_session()['_flash']
+        return flash_msg
+
 
 class LoginHandler(BaseHandler):
     def get(self):
+        try:
+            self.response.out.write("<div class='success'>%s</div>" % self.get_flash())
+        except:
+            self.response.out.write("<p>Please login:</p>")
+
         return """
-			<!DOCTYPE hml>
-			<html>
-				<head>
-					<title>webapp2 auth example</title>
-				</head>
-				<body>
-				<form action="%s" method="post">
-					<fieldset>
-						<legend>Login form</legend>
-						<label>Username <input type="text" name="username" placeholder="Your username" /></label>
-						<label>Password <input type="password" name="password" placeholder="Your password" /></label>
-						<label>Remember me? <input type="checkbox" name="remember_me" placeholder="Remember me?" /></label>
-					</fieldset>
-					<button>Login</button>
-				</form>
-			</html>
+<!DOCTYPE hml>
+<html>
+    <head>
+        <title>webapp2 auth</title>
+    </head>
+    <body>
+    <form action="%s" method="post">
+        <fieldset>
+            <legend>Login form</legend>
+            <label>Username <input type="text" name="username" placeholder="Your username" /></label>
+            <label>Password <input type="password" name="password" placeholder="Your password" /></label>
+            <label>Remember me? <input type="checkbox" name="remember_me" placeholder="Remember me?" /></label>
+        </fieldset>
+        <button>Login</button>
+    </form>
+</html>
         """ % self.request.url
 
     def post(self):
-        
         username = self.request.POST.get("username")
         password = self.request.POST.get("password")
         remember_me = True if self.request.POST.get('remember_me') == 'on' else False
 
         try:
             self.auth.get_user_by_password(username, password, remember = remember_me)
-            self.redirect('/secure')
+            self.set_flash("You've logged in!")
+            self.redirect(self.uri_for("secure"))
         except (InvalidAuthIdError, InvalidPasswordError), e:
             return "Login error. Try again: <a href='%s'>Login</a>" % (self.auth_config['login_url'])
 
 class CreateUserHandler(BaseHandler):
     def get(self):
-
         return """
-			<!DOCTYPE hml>
-			<html>
-				<head>
-					<title>webapp2 auth example</title>
-				</head>
-				<body>
-				<form action="%s" method="post">
-					<fieldset>
-						<legend>Create user form</legend>
-						<label>Username <input type="text" name="username" placeholder="Your username" /></label>
-						<label>Email <input type="text" name="email" placeholder="Your email" /></label>
-						<label>Password <input type="password" name="password" placeholder="Your password" /></label>
-					</fieldset>
-					<button>Create user</button>
-				</form>
-			</html>
-        """ % self.request.url
+<!DOCTYPE hml>
+<html>
+    <head>
+        <title>webapp2 auth</title>
+    </head>
+    <body>
+    <form action="%s" method="post">
+        <fieldset>
+            <legend>Create user form</legend>
+            <label>Username <input type="text" name="username" placeholder="Your username" /></label>
+            <label>Email <input type="text" name="email" placeholder="Your email" /></label>
+            <label>Password <input type="password" name="password" placeholder="Your password" /></label>
+        </fieldset>
+        <button>Create user</button>
+    </form>
+</html>
+        """ % self.request.url 
 
     def post(self):
         username = self.request.POST.get('username')
@@ -124,11 +128,8 @@ class CreateUserHandler(BaseHandler):
         if not user[0]: #returns a tuple with [boolean, user_info]
             return 'Create user error: %s' % str(user)
         else:
-            # User is created, redirect to login page
-            try:
-                self.redirect(self.auth_config['login_url'], abort=True)
-            except (AttributeError, KeyError), e:
-                self.abort(403)
+            self.set_flash("Thank you for registering. Please login!")
+            self.redirect(self.auth_config['login_url'])
 
 
 class LogoutHandler(BaseHandler):
@@ -142,14 +143,11 @@ class LogoutHandler(BaseHandler):
             return "User is logged out"
 
 class SecureRequestHandler(BaseHandler):
-
     @user_required
     def get(self, **kwargs):
+        self.response.out.write("<div class='success'>%s</div>" % self.get_flash())
         user_session = self.auth.get_user_by_session()
         user = self.auth.store.user_model.get_by_auth_token(user_session['user_id'], user_session['token'])
-        user[0].username = 'a'
-        user[0].put()
-
         try:
             return "Secure zone %s <a href='%s'>Logout</a>" % (user, self.auth_config['logout_url'])
         except (AttributeError, KeyError), e:
@@ -165,9 +163,9 @@ class DisplayPropHandler(BaseHandler):
         else:
             return "Not found"
 
-app = webapp2.WSGIApplication([RedirectRoute('/login/', LoginHandler, name='login'),
-                               RedirectRoute('/logout/', LogoutHandler, name='logout'), 
-                               RedirectRoute('/secure', SecureRequestHandler, name='secure'),
-                               RedirectRoute('/display', DisplayPropHandler, name='display'),
-                               RedirectRoute('/', CreateUserHandler, name='create-user')], 
+app = webapp2.WSGIApplication([webapp2.Route('/login/', LoginHandler, name='login'),
+                               webapp2.Route('/logout/', LogoutHandler, name='logout'), 
+                               webapp2.Route('/secure', SecureRequestHandler, name='secure'),
+                               webapp2.Route('/display', DisplayPropHandler, name='display'),
+                               webapp2.Route('/', CreateUserHandler, name='create-user')], 
                                debug=True, config=config.webapp2_config)
---
+++ readme.md
@@ -0,0 +1,2 @@
+Simple webapp demonstrating authorization and authentication using `webapp2_extras auth` and `webapp2_extra sessions.` 
+[Demo](http://webapp-auth.appspot.com).Relevant blog post [here](http://blagonudacity.appspot.com/post/1)
